51432b93d14fa767d396ed8d84627d7f09613372
Ayesha Akhtar <ayeshakhtar2233@gmail.com>
2025-07-11 20:26:29 +0500

    User Panel Buttons

--- resources/views/user_dashboard/usersidebar.blade.php
+++ resources/views/user_dashboard/usersidebar.blade.php
@@ -11,7 +11,7 @@
             <i class="fa-solid fa-bars"></i>
         </button>
         <div class="vertical-nav mt-4" id="sidebar">
-            <div class="py-4 mb-4">
+            <div class="py-3 mb-4">
                 <div class="media d-flex align-items-center">
                     <i class="fa-solid fa-user me-2 fs-5"></i>
                     @auth
@@ -23,26 +23,26 @@
                 </div>
 
 
-                <h4 class="p-3 mb-0">User</h4>
+                <h4 class="py-2 mb-0">User</h4>
                 <ul class="nav flex-column">
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.myfeed') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.myfeed') }}" id="bar"><i
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.myfeed') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.myfeed') }}" id="sidemenu"><i
                                 class="fa-solid fa-house"></i> My Feed</a></li>
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.myreadingstat') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.myreadingstat') }}" ><i
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.myreadingstat') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.myreadingstat') }}" id="sidemenu"><i
                                 class="fa-solid fa-square-poll-vertical"></i> My Reading Stat</a></li>
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.mycalendar') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.mycalendar') }}" ><i
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.mycalendar') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.mycalendar') }}" id="sidemenu"><i
                                 class="fa-solid fa-calendar-days"></i> My Calendar</a></li>
 
-                    <h4 class="p-3 mb-0">Reading Log</h4>
-                    
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.wishlist') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.wishlist') }}"><i
+                    <h4 class="py-2 mb-0">Reading Log</h4>
+
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.wishlist') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.wishlist') }}" id="sidemenu"><i
                                 class="fa-solid fa-book-open"></i> Wishlist</a></li>
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.currentlyread') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.currentlyread') }}" ><i
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.currentlyread') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.currentlyread') }}" id="sidemenu"><i
                                 class="fa-solid fa-book-open-reader"></i> Currently Read</a></li>
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.alreadyread') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.alreadyread') }}" ><i
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.alreadyread') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.alreadyread') }}" id="sidemenu"><i
                                 class="fa-solid fa-check-double"></i> Already Read</a></li>
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.mynotes') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.mynotes') }}" ><i
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.mynotes') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.mynotes') }}" id="sidemenu"><i
                                 class="fa-solid fa-note-sticky"></i> My Notes</a></li>
-                    <li class="nav-item  {{ request()->routeIs('user_dashboard.myreviews') ? 'active' : '' }}" id="sidemenu"><a href="{{ route('user_dashboard.myreviews') }}"><i
+                    <li class="nav-item  {{ request()->routeIs('user_dashboard.myreviews') ? 'active' : '' }}" id="sidebuttons"><a href="{{ route('user_dashboard.myreviews') }}" id="sidemenu"><i
                                 class="fa-solid fa-star"></i> My Reviews</a></li>
                 </ul>
             </div>
