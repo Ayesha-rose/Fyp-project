fd6ab8bd79dfa61bcfe6b0bf9a9f75cadd738b3b
Ayesha Akhtar <ayeshakhtar2233@gmail.com>
2025-07-16 19:34:37 +0500

    Logo

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -15,7 +15,7 @@
     <nav class="navbar navbar-expand-lg navbar-light mx-0 px-0">
       <div class="container">
         <a class="navbar-brand" href="#">
-          <img src="images/Logo.png" alt="Logo" style="max-height: 40px;">
+          <img src="../images/Logo.png" alt="Logo" style="max-height: 40px;">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span class="navbar-toggler-icon"></span>
